3d8971e4051be16b8911ad0f38f6f601f5ed40af
EkaterinaRusakova17 <145272537+EkaterinaRusakova17@users.noreply.github.com>
2023-10-03 16:35:10 +0700

    Create lab2postcondition.cpp

---
+++ lab2postcondition.cpp
@@ -0,0 +1,30 @@
+/************************************************************
+* Русакова Екатерина Алексеевна, ПИ-231, вариант №16        *
+* Share-Link: https://www.onlinegdb.com/online_c++_compiler *
+* Тема: Программирование циклов с пред- и постусловием      *
+*************************************************************/
+
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main() {
+  double H, h, v;
+
+  cout << "Введите H= ";
+  cin >> H;
+  cout << "Введите h= ";
+  cin >> h;
+  cout << "Введите v= ";
+  cin >> v;
+
+  do {
+  double V = v * ( (log (H / h) * log (H / h) ) / (log (10 / h) * log (10 / h) ) );
+  cout << "H= " << H << " V= " << V << endl;
+  H += 2; } 
+  while ( (H > 2) && (H < 90) );
+  
+  system ("pause");
+  return 0;
+}
